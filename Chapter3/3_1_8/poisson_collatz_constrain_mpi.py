6e292665231d0fb8eaf5e779283fd58b2a1dbdd5
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2026-02-11 20:47:27 +0000

    testing png diff

--- Chapter3/3_1_8/poisson_collatz_constrain_mpi.py
+++ Chapter3/3_1_8/poisson_collatz_constrain_mpi.py
@@ -9,6 +9,8 @@ from devito.mpi.distributed import MPI
 from devito.petsc import petscsolve, EssentialBC
 from devito.petsc.initialize import PetscInitialize
 
+import matplotlib
+matplotlib.use("Agg")  # Fully deterministic non-interactive backend
 import matplotlib.pyplot as plt
 
 configuration['compiler'] = 'custom'
@@ -218,4 +220,4 @@ if comm.rank == 0:
     plt.title(f'Convergence Plot (MPI processes = {size})')
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"3_1_8_constrain_mpi_procs{size}.png", dpi=200)
+    plt.savefig(f"3_1_8_constrain_mpi_procs{size}.png", dpi=200, metadata={})
